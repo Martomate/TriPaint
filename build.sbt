087d9dc8b51bb4f1ba181fe2eb8959770121db2f
Martomate <martinnn97@hotmail.com>
2018-11-10 15:16:51 +0800

    Configured the release packaging system so that it works

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ name := "TriPaint"
 organization := "com.martomate"
 version := "1.1-SNAPSHOT"
 
+enablePlugins(LauncherJarPlugin)
+
 libraryDependencies ++= Seq(
   "org.scalactic" %% "scalactic" % "3.0.5",
   "org.scalatest" %% "scalatest" % "3.0.5" % "test",
--- src/main/scala/com/martomate/tripaint/plan.scala
+++ src/main/scala/com/martomate/tripaint/plan.scala
@@ -4,6 +4,8 @@ import com.martomate.tripaint.image.format.StorageFormat
 import com.martomate.tripaint.image.storage.ImageStorage
 import scalafx.scene.canvas.Canvas
 
+import scala.collection.mutable.ArrayBuffer
+
 object plan {
   private object model {
     object image {
@@ -28,6 +30,33 @@ object plan {
     trait ImageGridPane {
 //      val collection: ImageGrid
       val imagePanes: Seq[ImagePane]
+    }
+  }
+
+  object listenableWithHandle {
+    abstract class Listenable[L] {
+      protected val listeners: ArrayBuffer[L] = ArrayBuffer.empty
+
+      final def addListener(listener: L): Unit = listeners += listener
+
+      final def removeListener(listener: L): Unit = listeners -= listener
+    }
+
+    class ListenableHandle[L] extends Listenable[L] {
+      final def notifyListeners(func: L => Unit): Unit = listeners.foreach(func)
+    }
+
+    class TestListenableClass {
+      def somethingInteresting: Listenable[Int => String] = somethingInterestingHandle
+
+      private val somethingInterestingHandle = new ListenableHandle[Int => String]
+      somethingInterestingHandle.notifyListeners(_ (7))
+    }
+
+    trait TestListenableClassUser {
+      val listenable: TestListenableClass
+      listenable.somethingInteresting.addListener(a => "hello " + a)
+      listenable.somethingInteresting.addListener(a => "hello again " + a)
     }
   }
 }
