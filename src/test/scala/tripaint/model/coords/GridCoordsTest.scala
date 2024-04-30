cd17f8b3f064fe7cf4405a22477422aec7156206
Martin Jakobsson <martin@backtick.se>
2024-04-30 16:39:21 +0200

    Fixed major performance issue

--- src/test/scala/tripaint/model/coords/GridCoordsTest.scala
+++ src/test/scala/tripaint/model/coords/GridCoordsTest.scala
@@ -8,24 +8,24 @@ class GridCoordsTest extends FunSuite {
   private val aThirdHeight: Double = unitHeight / 3
 
   test("(xOff, yOff) should be the mass center of the triangle") {
-    assertEqualsDouble(GridCoords(0, 0).centerX, 0.5, epsilon)
-    assertEqualsDouble(GridCoords(0, 3).centerX, 3 * 0.5 + 0.5, epsilon)
-    assertEqualsDouble(GridCoords(0, -3).centerX, -3 * 0.5 + 0.5, epsilon)
+    assertEqualsDouble(GridCoords(0, 0).center._1, 0.5, epsilon)
+    assertEqualsDouble(GridCoords(0, 3).center._1, 3 * 0.5 + 0.5, epsilon)
+    assertEqualsDouble(GridCoords(0, -3).center._1, -3 * 0.5 + 0.5, epsilon)
 
-    assertEqualsDouble(GridCoords(1, 0).centerX, 1.0, epsilon)
-    assertEqualsDouble(GridCoords(7, 3).centerX, 3 * 0.5 + 4, epsilon)
-    assertEqualsDouble(GridCoords(7, -3).centerX, -3 * 0.5 + 4, epsilon)
-    assertEqualsDouble(GridCoords(-7, 3).centerX, 3 * 0.5 + -3, epsilon)
-    assertEqualsDouble(GridCoords(-7, -3).centerX, -3 * 0.5 + -3, epsilon)
+    assertEqualsDouble(GridCoords(1, 0).center._1, 1.0, epsilon)
+    assertEqualsDouble(GridCoords(7, 3).center._1, 3 * 0.5 + 4, epsilon)
+    assertEqualsDouble(GridCoords(7, -3).center._1, -3 * 0.5 + 4, epsilon)
+    assertEqualsDouble(GridCoords(-7, 3).center._1, 3 * 0.5 + -3, epsilon)
+    assertEqualsDouble(GridCoords(-7, -3).center._1, -3 * 0.5 + -3, epsilon)
 
-    assertEqualsDouble(-GridCoords(0, 0).centerY, aThirdHeight, epsilon)
-    assertEqualsDouble(-GridCoords(0, 3).centerY, 3 * unitHeight + aThirdHeight, epsilon)
-    assertEqualsDouble(-GridCoords(0, -3).centerY, -3 * unitHeight + aThirdHeight, epsilon)
+    assertEqualsDouble(-GridCoords(0, 0).center._2, aThirdHeight, epsilon)
+    assertEqualsDouble(-GridCoords(0, 3).center._2, 3 * unitHeight + aThirdHeight, epsilon)
+    assertEqualsDouble(-GridCoords(0, -3).center._2, -3 * unitHeight + aThirdHeight, epsilon)
 
-    assertEqualsDouble(-GridCoords(1, 0).centerY, 2 * aThirdHeight, epsilon)
-    assertEqualsDouble(-GridCoords(7, 3).centerY, 3 * unitHeight + 2 * aThirdHeight, epsilon)
-    assertEqualsDouble(-GridCoords(7, -3).centerY, -3 * unitHeight + 2 * aThirdHeight, epsilon)
-    assertEqualsDouble(-GridCoords(-7, 3).centerY, 3 * unitHeight + 2 * aThirdHeight, epsilon)
-    assertEqualsDouble(-GridCoords(-7, -3).centerY, -3 * unitHeight + 2 * aThirdHeight, epsilon)
+    assertEqualsDouble(-GridCoords(1, 0).center._2, 2 * aThirdHeight, epsilon)
+    assertEqualsDouble(-GridCoords(7, 3).center._2, 3 * unitHeight + 2 * aThirdHeight, epsilon)
+    assertEqualsDouble(-GridCoords(7, -3).center._2, -3 * unitHeight + 2 * aThirdHeight, epsilon)
+    assertEqualsDouble(-GridCoords(-7, 3).center._2, 3 * unitHeight + 2 * aThirdHeight, epsilon)
+    assertEqualsDouble(-GridCoords(-7, -3).center._2, -3 * unitHeight + 2 * aThirdHeight, epsilon)
   }
 }
