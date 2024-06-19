cae0f2cc7235681741c56a84e8befd283e0cb94d
Martin Jakobsson <martin@backtick.se>
2024-06-19 20:15:21 +0200

    Refactor: Added explict main method

--- app/src/main/scala/tripaint/TriPaint.scala
+++ app/src/main/scala/tripaint/TriPaint.scala
@@ -3,13 +3,20 @@ package tripaint
 import scalafx.application.{JFXApp3, Platform}
 import scalafx.application.JFXApp3.PrimaryStage
 
-object TriPaint extends JFXApp3 {
-  override def start(): Unit = {
-    val model: TriPaintModel = TriPaintModel.create()
-    val controller = new TriPaintController(model, new MainStage(_, _))
-    stage = controller.view.asInstanceOf[PrimaryStage]
-    Platform.runLater(
-      model.imageGrid.setImageSizeIfEmpty(controller.view.askForImageSize().getOrElse(32))
-    )
+object TriPaint {
+  def main(args: Array[String]): Unit = {
+    // By creating a custom main method it becomes possible to perform init code before the app starts
+    TriPaint.App.main(args)
+  }
+
+  private object App extends JFXApp3 {
+    override def start(): Unit = {
+      val model: TriPaintModel = TriPaintModel.create()
+      val controller = new TriPaintController(model, new MainStage(_, _))
+      stage = controller.view.asInstanceOf[PrimaryStage]
+      Platform.runLater(
+        model.imageGrid.setImageSizeIfEmpty(controller.view.askForImageSize().getOrElse(32))
+      )
+    }
   }
 }
